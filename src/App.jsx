e1b0c8d214339f8529f74393c0ec9ee73759e62d
Isaque Pereira dos Santos <isaquevidaltube@gmail.com>
2025-07-20 14:23:46 -0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.scss';
 import { Sum } from './components/Sum/Sum';
 
 export const App = () => (
-  <>
+  <div className="app">
     <Sum a={2} b={3} />
     <Sum a={-5} b={-5} />
     <Sum a={10} b={0} />
     <Sum a={0} b={5} />
     <Sum a={0} b={0} />
-  </>
+  </div>
 );
